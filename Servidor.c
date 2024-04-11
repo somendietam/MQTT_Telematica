2786781489c697192914fba87180480903e0cb24
Sofía <79530549+somendietam@users.noreply.github.com>
2024-04-11 11:29:56 -0500

    Construcción Connack
    Construcción paquete connack

--- Servidor.c
+++ Servidor.c
@@ -7,6 +7,22 @@
 #define SERVER_PORT 1883
 #define BUFFER_SIZE 1024
 
+struct connack_fixed_header{
+    char control_packet_type;
+    char remaining_length;
+}connack_fixed_header;
+
+struct connack_variable_header{
+    char flags;
+    char return_code;
+}connack_variable_header;
+
+//Definir funciones
+void printbuffer(char arr[]);
+void build_connack(int cliente_socket, struct connack_fixed_header *fixed_header, struct connack_variable_header *variable_header, int flag_clean_ssn);
+int search_flag_connect(char buffer[]);
+
+
 int main() {
     int servidor_socket, cliente_socket;
     struct sockaddr_in servidor_addr, cliente_addr;
@@ -49,29 +65,123 @@ int main() {
     printf("Conexión establecida con el cliente\n");
 
     // Bucle para recibir mensajes del cliente
-    while (1) {
-        memset(buffer, 0, BUFFER_SIZE); // Limpiar el buffer
+    memset(buffer, 0, BUFFER_SIZE); // Limpiar el buffer
 
-        // Recibir mensaje del cliente
-        ssize_t bytes_recibidos = recv(cliente_socket, buffer, BUFFER_SIZE, 0);
-        if (bytes_recibidos <= 0) {
-            perror("Error al recibir mensaje del cliente");
-            break;
+    // Recibir mensaje del cliente
+    ssize_t bytes_recibidos = recv(cliente_socket, buffer, BUFFER_SIZE, 0);
+    if (bytes_recibidos <= 0) {
+        perror("Error al recibir mensaje del cliente");
+    }
+
+    // Mostrar mensaje recibido
+    printbuffer(buffer);
+    // Verificar si el cliente quiere salir
+    printf("\n");
+    printf("\n");
+
+    int flag = search_flag_connect(buffer);
+
+    build_connack(servidor_socket, &connack_fixed_header, &connack_variable_header, flag);
+
+
+    printf("FIN DEL MAIN\n");
+
+    return 0;
+}
+
+
+void printbuffer(char arr[]) {
+    printf("\n");
+    for (int i = 0; i<BUFFER_SIZE; ++i) {
+        char c = arr[i];
+        for (int j = 7; j >= 0; --j) {
+            putchar((c & (1 << j)) ? '1' : '0');
         }
+        putchar(' ');
+    }
+    putchar('\n');
+}
 
-        // Mostrar mensaje recibido
-        printf("Mensaje recibido del cliente: %s\n", buffer);
 
-        // Verificar si el cliente quiere salir
-        if (strcmp(buffer, "salir\n") == 0) {
-            printf("El cliente ha solicitado salir. Cerrando conexión...\n");
-            break; // Salir del bucle
+int search_flag_connect(char buffer[]){
+   int contador = 0;
+    
+    for (int i = 1; i <= 4; i++) {
+        // Comprobamos el primer bit del byte actual
+        if (buffer[i] & 0x80) { // 0x80 es 10000000 en binario
+            // El primer bit es 1
+            contador++;
+        } else {
+            // El primer bit es 0, salimos del ciclo
+            break;
         }
     }
 
-    // Cerrar socket del cliente y del servidor cuando termines
-    close(cliente_socket);
-    close(servidor_socket);
+    int pos_flag_connect = (contador + 10)-1; // Sumamos 2 al contador ya que tenemos el byte de fixed header y el ultimo Byte del remaining length que era el que comenzaba en 1
+    //y sumamos 8 que son los siguientes bytes de protocolo mqtt, en total 2+8=10
+    
+    char bin_flag_connect = buffer[pos_flag_connect];
 
-    return 0;
+    printf("El valor de posicion_flag es: %d\n", pos_flag_connect);
+
+    int flag_clean_ssn = 0;
+
+    // Desplazar el bit 1 posición a la derecha para acceder al bit deseado
+    char bit = (bin_flag_connect >> 1) & 1;
+
+    // Imprimir el resultado
+    if (bit == 1)
+        flag_clean_ssn = 1;
+
+    printf("el flag clean session es: %d\n", flag_clean_ssn);
+    //printf("El carácter es: %c\n", bin_flag_connect);
+    // Iterar sobre cada bit del byte (char)
+    for (int i = 7; i >= 0; i--) {
+
+        // Verificar si el bit en la posición i está encendido
+        if (bin_flag_connect & (1 << i))
+            printf("1");
+        else
+            printf("0");
+    }
+    printf("\n");
+
+    return flag_clean_ssn;
+}
+
+
+void build_connack(int cliente_socket, struct connack_fixed_header *fixed_header, struct connack_variable_header *variable_header, int flag_clean_ssn){
+    char buffer_connack[4];
+    connack_fixed_header.control_packet_type = 0x20;
+    connack_fixed_header.remaining_length = 0x02;
+    
+    if (flag_clean_ssn == 1){
+        connack_variable_header.flags = 0x00;
+    }else{
+        connack_variable_header.flags = 0x01;
+    }
+
+    connack_variable_header.return_code = 0x00;
+
+
+    buffer_connack[0] = connack_fixed_header.control_packet_type;
+    buffer_connack[1] = connack_fixed_header.remaining_length;
+    buffer_connack[2] = connack_variable_header.flags;
+    buffer_connack[3] = connack_variable_header.return_code;
+
+    printf("Este es el BUFFER CONNACK: \n");
+    printbuffer(buffer_connack);
+    printf("\n");
+
+    ssize_t sizebufferconnack = sizeof(buffer_connack);
+    printf("Este es el sizeBUFFERCONNACK: %zu\n", sizebufferconnack);
+
+    // Enviar el paquete CONNECT al servidor
+    printf("Este es el sizeBUFFERCONNACK: %zu\n", sizebufferconnack);
+
+    send(cliente_socket, buffer_connack, sizebufferconnack, 0);
+    
+    printf("Paquete CONNACK enviado al cliente.\n");
+
+    
 }
